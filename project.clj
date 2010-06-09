9645cbd4c5bb586befa9910ddedacb9c8648fa17
Brenton Ashworth <bashwort@gmail.com>
2010-06-09 13:15:51 -0700

    Increment sandbar version and update README.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject sandbar "0.2.3"
+(defproject sandbar "0.2.4"
   :description "Clojure web application libraries built on top of Ring
                 and Compojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
                  [ring/ring-jetty-adapter "0.2.0"]
                  [sandbar/sandbar-core "0.3.0"]
-                 [sandbar/sandbar-session "0.2.3"]
+                 [sandbar/sandbar-session "0.2.4"]
                  [sandbar/sandbar-auth "0.2.3"]
                  [sandbar/sandbar-dev "0.0.1-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
