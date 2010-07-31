84f55a3d4dc14f90c8f67ddf99512ddce1e23ff8
Brenton Ashworth <bashwort@gmail.com>
2010-07-31 10:47:15 -0700

    Use Clojure 1.2-RC1.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject sandbar/sandbar "0.3.0-SNAPSHOT"
   :description "Clojure web application libraries built on top of Ring
                 and Compojure."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
                  [compojure "0.4.0"]
                  [hiccup "0.2.6"]
-                 [inflections "0.3"]]
+                 [inflections "0.4-SNAPSHOT"]]
   :dev-dependencies [[jline "0.9.94"]
                      [ring/ring-devel "0.2.3"]
                      [ring/ring-jetty-adapter "0.2.3"]
